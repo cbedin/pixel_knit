062cb2efed0f691a4d5e5307116b934a5a4039df
Cooper Bedin <cooper.bedin@berkeley.edu>
2022-07-25 18:16:12 -0700

    support for swatch sizes and instructions

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
-from convert_image import convert_to_pixels
+from convert_image import convert_to_pixels, convert_to_instructions
 
 app = Flask(__name__)
 IN_FOLDER = "imgs"
@@ -10,18 +10,23 @@ OUT_FOLDER = "imgs_out"
 def upload():
     return render_template('upload.html')
 	
-@app.route('/convert', methods = ['POST'])
+@app.route('/convert', methods=['GET', 'POST'])
 def convert():
     if request.method == 'POST':
         f = request.files['file']
-        num_stitches = int(request.form.get('num_stitches'))
+        stitch_width = float(request.form.get('stitch_width'))
+        stitch_height = float(request.form.get('stitch_height'))
+        proj_width = float(request.form.get('proj_width'))
         num_colors = int(request.form.get('num_colors'))
         in_fname = secure_filename(f.filename)
         f.save(f"static/{IN_FOLDER}/{in_fname}")
-        out_arr, out_fname = convert_to_pixels(in_fname, num_stitches, num_colors, IN_FOLDER, OUT_FOLDER)
+        out_arr, out_fname = convert_to_pixels(in_fname, stitch_width, stitch_height, proj_width, num_colors, IN_FOLDER, OUT_FOLDER)
         in_fname = f"{IN_FOLDER}/{in_fname}"
         out_fname = f"{OUT_FOLDER}/{out_fname}"
-        return render_template('converted.html', in_fname=in_fname, out_fname=out_fname)
+        instructions = convert_to_instructions(out_arr)
+        return render_template('converted.html', in_fname=in_fname, out_fname=out_fname, instructions=instructions)
+    else:
+        return redirect(url_for('upload'))
 
 if __name__ == "__main__":
     app.run()
--- convert_image.py
+++ convert_image.py
@@ -4,46 +4,34 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from collections import Counter
 
-def convert_to_pixels(infile_name, num_stitches_wide, num_colors, in_folder, out_folder):
+def convert_to_pixels(infile_name, stitch_width, stitch_height, proj_width, colors, in_folder, out_folder):
     pic = imageio.imread(f"static/{in_folder}/{infile_name}")
-    box_size = pic.shape[1] // num_stitches_wide
     pic_fit = np.reshape(pic, (-1, pic.shape[-1]))
-    kmeans = KMeans(n_clusters=num_colors, random_state=0).fit(pic_fit)
+    kmeans = KMeans(n_clusters=colors, random_state=0).fit(pic_fit)
     centers = np.rint(kmeans.cluster_centers_).astype('uint8')
-
     new_pic = np.asarray([centers[i] for i in kmeans.labels_])
     new_pic = np.reshape(new_pic, pic.shape)
-    out_arr = []
-    for row in range(0, new_pic.shape[0], box_size):
-        out_arr.append([])
-        for col in range(0, new_pic.shape[1], box_size):
-            out_arr[-1].append([])
-            for color in range(new_pic.shape[2]):
-                slice = new_pic[row:row + box_size, col:col + box_size, color]
-                c = Counter(slice.flatten())
-                out_arr[-1][-1].append(c.most_common()[0][0])
-    out_arr = np.asarray(out_arr)
-    diff = (out_arr.shape[1] - num_stitches_wide) // 2
-    out_arr = out_arr[:, diff:num_stitches_wide + diff, :]
-    pic_arr = []
-    for row in out_arr:
-        pic_arr.append([])
-        for i in row:
-            pic_arr[-1].extend([i] * box_size)
-        pic_arr.extend([pic_arr[-1]] * (box_size - 1))
-    pic_arr = np.asarray(pic_arr)
+    block_width = round(pic.shape[1] / (proj_width / stitch_width))
+    block_height = round(pic.shape[0] / (proj_width * pic.shape[1] / pic.shape[0] / stitch_height))
+    for row in range(0, new_pic.shape[0], block_height):
+        for col in range(0, new_pic.shape[1], block_width):
+            slice = new_pic[row:row + block_height, col:col + block_width, :]
+            old_shape = slice.shape
+            slice = np.reshape(slice, (-1, slice.shape[-1]))
+            c = Counter(map(tuple, slice))
+            new_slice = np.full(old_shape, np.asarray(c.most_common()[0][0]))
+            new_pic[row:row + block_height, col:col + block_width, :] = new_slice
     fname = infile_name[:infile_name.index('.')]
     out_fname = f'{fname}_out.png'
-    plt.imsave(f'static/{out_folder}/{out_fname}', pic_arr)
-    return out_arr, out_fname
+    plt.imsave(f'static/{out_folder}/{out_fname}', new_pic)
+    return new_pic[::block_height, ::block_width, :], out_fname
 
-def convert_to_instructions(infile_name, num_stitches_wide, num_colors, out_folder):
-    # DEAL WITH LATER
-    f = open(f'static/instructions_out/{fname}_instructions.txt', 'w')
+def convert_to_instructions(out_arr):
+    instructions = ""
     color_map = {}
     color_count = 0
     for row in range(out_arr.shape[0]):
-        f.write(f'Row {row}:\t')
+        instructions += f'Row {row}:\t'
         tracking_color = None
         count = 0
         for col in range(out_arr.shape[1]):
@@ -57,14 +45,14 @@ def convert_to_instructions(infile_name, num_stitches_wide, num_colors, out_fold
                 tracking_color = curr_color
                 count = 1
             elif col == out_arr.shape[1] - 1:
-                f.write(f'{count + 1} st {color_map[tracking_color]}\n')
+                instructions += f'{count + 1} st {color_map[tracking_color]}<br>'
             elif curr_color != tracking_color:
-                f.write(f'{count} st {color_map[tracking_color]}, ')
+                instructions += f'{count} st {color_map[tracking_color]}, '
                 tracking_color = curr_color
                 count = 1
             else:
                 count += 1
 
-    f.write('\n')
-    f.writelines([f"{name}: {color}\n" for color, name in color_map.items()])
-    f.close()
+    instructions += "<br>"
+    instructions += '<br>'.join([f"Color {name}: {color}" for color, name in color_map.items()])
+    return instructions
